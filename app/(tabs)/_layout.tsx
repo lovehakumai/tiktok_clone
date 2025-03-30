edb3bae28e3d84cd79465bcedd467baf17239d37
lovehakumai <masaotomeza@gmail.com>
2025-03-30 09:10:44 +0900

    first commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function TabLayout() {
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#000',
+        headerShown: false
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ()=><Text>Home</Text>
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ()=><Text>Explore</Text>
+        }}
+      />
+    </Tabs>
+  );
+}
