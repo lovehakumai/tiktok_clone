473b527bee5eb6780b70da1a646712f8f34fa6e7
lovehakumai <masaotomeza@gmail.com>
2025-04-02 20:37:59 +0900

    change camera text

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function camera() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='font-bold text-3xl'>Camera</Text>
+      <Text className='font-bold text-3xl'>My Camera</Text>
     </View>
   );
 }
